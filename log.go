4e87e88287a423da18f0b1d6c2eb3d02d758593a
aurora.io <zhiwen_der@qq.com>
2022-09-10 18:34:11 +0800

    update

--- log.go
+++ log.go
@@ -20,13 +20,18 @@ type Formatter struct {
 
 func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	var buf *bytes.Buffer
+	format := ""
 	if entry.Buffer != nil {
 		buf = entry.Buffer
 	} else {
 		buf = &bytes.Buffer{}
 	}
 	t := entry.Time.Format("2006-01-02 15:04:05")
-	format := fmt.Sprintf("%s [%s] -> %s\n", t, entry.Level, entry.Message)
+	if entry.Data != nil && len(entry.Data) > 0 {
+		format = fmt.Sprintf("%s [%s] [%s]-> %s\n", t, entry.Level, entry.Data["type"], entry.Message)
+	} else {
+		format = fmt.Sprintf("%s [%s] -> %s\n", t, entry.Level, entry.Message)
+	}
 	buf.WriteString(format)
 	return buf.Bytes(), nil
 }
